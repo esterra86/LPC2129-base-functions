045dc9c2fcb5901ef196eb64f55ffbaff7d3fc04
laptop <laptop@gamil.com>
2025-02-17 21:45:28 +0100

    automaty

---
+++ keyboard.h
@@ -0,0 +1,4 @@
+enum KeyboardState {BUTTON_0, BUTTON_1, BUTTON_2, BUTTON_3,RELEASED};
+
+//void KeyboardInit(void);
+enum KeyboardState eKeyboardRead(void);
---
+++ led.h
@@ -0,0 +1,9 @@
+enum Direction {LEFT, RIGHT};
+
+void LedOn(unsigned char ucLedIndeks);
+//void LedStep(enum Direction eDirection);
+void LedInit(void);
+void LedStepLeft(void);
+void LedStepRight(void);
+
+
